38404f5d0af04dde0c11764687b43c22adb8db52
wirelyre <wirelyre@gmail.com>
2021-07-09 16:51:33 -0400

    Add reachability for simple graphs

--- src/boardgraph/simple.rs
+++ src/boardgraph/simple.rs
@@ -1,7 +1,49 @@
 use rayon::prelude::*;
 
 use super::Stage;
-use crate::{boardgraph::PiecePlacer, gameplay::Shape};
+use crate::{
+    boardgraph::PiecePlacer,
+    gameplay::{Board, Shape},
+};
+
+pub struct SimpleGraph(pub Vec<SimpleStage>);
+
+impl SimpleGraph {
+    pub fn compute() -> SimpleGraph {
+        let mut forward_stages = Vec::new();
+        forward_stages.push(SimpleStage::new());
+
+        for iter in 1..=4 {
+            forward_stages.push(forward_stages.last().unwrap().step());
+            println!(
+                "After iteration {}, have {} boards.",
+                iter,
+                forward_stages.last().unwrap().count_boards()
+            );
+        }
+
+        let mut backward_stages = Vec::new();
+        let mut target_stage = forward_stages
+            .pop()
+            .unwrap()
+            .filter(Board(0b0000000111_0000001111_0000011111_0000001111));
+
+        while let Some(stage) = forward_stages.pop() {
+            let this_stage = stage.target(&target_stage);
+            backward_stages.push(target_stage);
+            target_stage = this_stage;
+        }
+
+        backward_stages.push(target_stage);
+        backward_stages.reverse();
+
+        for (i, stage) in backward_stages.iter().enumerate() {
+            println!("After stage {}, have {} boards.", i, stage.count_boards());
+        }
+
+        SimpleGraph(backward_stages)
+    }
+}
 
 pub struct SimpleStage(pub Stage<()>);
 
@@ -30,5 +72,32 @@ impl SimpleStage {
 
     pub fn count_boards(&self) -> usize {
         self.0 .0.iter().map(|subset| subset.lock().len()).sum()
+    }
+
+    pub fn filter(&self, board: Board) -> SimpleStage {
+        assert!(self.0.lock_subset(board).get(&board).is_some());
+
+        let new_stage = SimpleStage(Stage::empty());
+        new_stage.0.lock_subset(board).insert(board, ());
+
+        new_stage
+    }
+
+    pub fn target(&self, target: &SimpleStage) -> SimpleStage {
+        let target = target.0.lock_all();
+        let new_stage = SimpleStage(Stage::empty());
+
+        self.0.lock_all().par_iter().for_each(|(&board, &())| {
+            for &shape in &Shape::ALL {
+                for (_, new_board) in PiecePlacer::new(board, shape) {
+                    if target.get(new_board).is_some() {
+                        new_stage.0.lock_subset(board).insert(board, ());
+                        return;
+                    }
+                }
+            }
+        });
+
+        new_stage
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
 pub mod boardgraph;
 pub mod gameplay;
 
-use std::io::{stdout, Write};
+use boardgraph::simple::SimpleGraph;
 
-use crate::boardgraph::gamestate::{GameStateStage, QuantumBag};
-
-fn main() -> std::io::Result<()> {
-    let mut stdout = stdout();
-
-    let mut stages = Vec::new();
-    stages.push(GameStateStage::new(QuantumBag::every_bag_no_hold()));
-
-    for iter in 1..=4 {
-        stages.push(stages.last().unwrap().step());
-
-        writeln!(
-            stdout,
-            "After iteration {}, have {} boards ({} bags).",
-            iter,
-            stages.last().unwrap().count_boards(),
-            stages.last().unwrap().count_bags(),
-        )?;
-    }
-
-    Ok(())
+fn main() {
+    let _graph = SimpleGraph::compute();
 }
